0b0b58ea898b0510738f5b82878b3d0e227583c2
yannick-art <yannick@icy-immobilier.fr>
2026-03-29 10:53:34 +0200

    Add files via upload

---
+++ form.js
@@ -0,0 +1,24 @@
+(function(){
+  window.envoyerFormulaire = function(formId){
+    var form = document.getElementById(formId);
+    if(!form) return;
+    var inputs = form.querySelectorAll('input,select,textarea');
+    var data = {source: formId, page: window.location.pathname};
+    inputs.forEach(function(el){
+      var key = el.id || el.name || el.placeholder;
+      if(key) data[key] = el.value;
+    });
+    var webhookUrl = 'WEBHOOK_MAKE_URL';
+    if(webhookUrl === 'WEBHOOK_MAKE_URL'){
+      alert('Merci pour votre demande. Yannick vous recontactera sous 24h.');
+      return;
+    }
+    fetch(webhookUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(function(){ alert('Merci ! Votre demande a bien ete envoyee. Reponse sous 24h.'); })
+    .catch(function(){ alert('Une erreur est survenue. Contactez-nous : contact@icy-immobilier.fr'); });
+  };
+})();
